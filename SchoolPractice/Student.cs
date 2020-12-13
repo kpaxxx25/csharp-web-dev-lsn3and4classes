398080a7df250f1170acc2e1d884a2ce476f26bc
Kirin Pax <kirin.ccb@gmail.com>
2020-12-13 17:20:50 -0600

    complete

--- SchoolPractice/Student.cs
+++ SchoolPractice/Student.cs
@@ -28,23 +28,64 @@ namespace SchoolPractice
         }
 
         // TODO: Complete the AddGrade method.
-        public void AddGrade(int courseCredits, double grade)
+        // Update the appropriate properties: NumberOfCredits, Gpa
+        public void AddGrade(int credits, double grade)
         {
-            // Update the appropriate properties: NumberOfCredits, Gpa
+            double qualityScore = Gpa * NumberOfCredits;
+            qualityScore += credits * grade;
+            NumberOfCredits += credits;
+            Gpa = qualityScore / NumberOfCredits;
         }
 
-        //TODO: Complete the GetGradeLevel method here:
+        //TODO: Complete the GetGradeLevel method here: 
+        // Determine the grade level of the student based on NumberOfCredits
         public string GetGradeLevel(int credits)
         {
-            // Determine the grade level of the student based on NumberOfCredits
-            return "grade level tbd";
+            if (credits >= 90)
+            {
+                return "Senior";
+            } else if (credits >= 60 && credits <= 89)
+            {
+                return "Junior";
+            } else if (credits >= 30 && credits <= 59)
+            {
+                return "Sophmore";
+            } else if (credits >= 0 && credits <= 29)
+            {
+                return "Freshman";
+            } else { 
+                return "grade level tbd";
+            }
         }
 
         // TODO: Add your custom 'ToString' method here. Make sure it returns a well-formatted string rather
         //  than just the class fields.
+        public override string ToString()
+        {
+            return $"{Name} (credits: {NumberOfCredits}, GPA: {Gpa})";
+        }
 
         // TODO: Add your custom 'Equals' method here. Consider which fields should match in order to call two
         //  Student objects equal.
+        public override bool Equals(object obj)
+        {
+            if(this == obj)
+            {
+                return true;
+            }
+            if(obj == null)
+            {
+                return false;
+            }
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
 
+            //turning 1 obj into another
+            //(Student)obj = obj as Student
+            Student other = obj as Student;
+            return this.StudentId == other.StudentId;
+        }
     }
 }
